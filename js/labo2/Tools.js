c06100eaf855e0f543da653208b84bb33e6f50ce
kim.biloni <kim.biloni@he-arc.ch>
2018-12-04 08:17:30 +0100

    add comments in index and labo2/tools.js

--- js/labo2/Tools.js
+++ js/labo2/Tools.js
@@ -1,4 +1,8 @@
-
+/**
+ * Generate a pseudo random number in a specify range
+ * @param {float} min minimum value
+ * @param {float} max maximum value
+ */
 function random(min, max)
 {
    let range = max - min;
